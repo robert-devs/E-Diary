24c6f20cd51983da384026a3b66f419794d22f48
robert <mainrobert04@gmail.com>
2023-04-13 19:02:49 +0300

    setup and adding server config

---
+++ server/controllers/post.js
@@ -0,0 +1,14 @@
+import postMessage from "../models/postMessage"
+
+
+export const getPosts= (req,res)=>{
+   try {
+    
+   } catch (error) {
+    
+   }
+}
+
+export const createPost = (req,res)=>{
+    res.send('Post Creation')
+}
